dd4e6b08fd65df5ab2011d1aff9fb643e2b0d2d4
Dekevion Wade <Dekevionwade@yahoo.com>
2019-08-14 16:06:13 -0500

    answered questions

---
+++ index.js
@@ -0,0 +1,54 @@
+let num = prompt("Enter a number (1-5)");
+    switch (num) {
+        case "1":
+            alert("1");
+            console.log("1");
+            break;
+        case"2":
+            alert("2");
+            console.log("2");
+            break;
+        case"3":
+            alert("3");
+            console.log("3");
+            break;
+        case"4":
+            alert("4");
+            console.log("4");
+            break;
+        case"5":
+            alert("5");
+            console.log("5");
+            break;
+        default:
+            alert("invalid entry");
+    }
+
+    // exercise 2
+
+let biNum = prompt("Enter a 3 digit binary number");
+    switch (biNum) {
+        case"1":
+            alert("1")
+            console.log("1");
+            break;
+        case"10":
+            alert("2");
+            console.log("2");
+            break;
+        case"11":
+            alert("3");
+            console.log("3");
+            break;
+        case"100":
+            alert("4");
+            console.log("4");
+            break;
+        case"101":
+            console.log("5");
+            alert("5");
+            break;
+        default:
+            alert("invalid entry");
+
+    }
